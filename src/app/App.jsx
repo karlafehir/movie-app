69eaa88a1c0ca26986211be6735c2343db1db588
karlafehir <fehirkarla@gmail.com>
2025-04-24 14:52:21 +0200

    added router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import FavoritesContainer from "../features/favorites/FavoritesContainer";
 import HomePageContainer from "../features/home/HomePageContainer";
+import Header from "../components/header/Header";
 import "./App.css";
 
 function App() {
   return (
-    <div className="p-10">
-      <HomePageContainer />
-    </div>
+    <Router>
+      <div className="flex flex-row">
+        <Routes>
+          <Route path="/" element={<HomePageContainer />}></Route>
+          <Route path="/favorites" element={<FavoritesContainer />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
