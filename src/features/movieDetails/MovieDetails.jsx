0749d289e5c32a996562dcef7152929abd476e97
karlafehir <fehirkarla@gmail.com>
2025-04-25 13:57:41 +0200

    movie genres fix

--- src/features/movieDetails/MovieDetails.jsx
+++ src/features/movieDetails/MovieDetails.jsx
@@ -5,16 +5,18 @@ const MovieDetails = ({ movie }) => {
     <>
       <div className="p-10">
         <MovieCardLargeContaier movie={movie} />
-        <div className="flex gap-4 p-4">
-          {movie.genres.map((genre) => (
-            <div
-              key={genre.key}
-              className="border-2 border-solid px-3 py-2 rounded-4xl cursor-pointer w-max"
-            >
-              {genre.name}
-            </div>
-          ))}
-        </div>
+        {movie.genres && (
+          <div className="flex gap-4 p-4">
+            {movie.genres.map((genre) => (
+              <div
+                key={genre.key}
+                className="border-2 border-solid px-3 py-2 rounded-4xl cursor-pointer w-max"
+              >
+                {genre.name}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
