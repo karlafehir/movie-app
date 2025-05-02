d0fa25354ed216b4bc234b443a8b51828ea702b9
karlafehir <fehirkarla@gmail.com>
2025-05-02 13:17:04 +0200

    reviewCard design

--- src/components/cards/reviewCard.jsx
+++ src/components/cards/reviewCard.jsx
@@ -1,21 +1,30 @@
-import { Avatar, Card } from "antd";
 import { format } from "date-fns";
+import { StarFilled } from "@ant-design/icons";
 
 const ReviewCard = ({ review }) => {
-  const { Meta } = Card;
-
   return (
-    <Card>
-      <Meta
-        avatar={
-          <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
-        }
-        title={review.author}
-        description={format(review.created_at, "dd.MM.yyyy.")}
-      />
-      <p className="text-red">{review.author_details.rating}</p>
-      <p className="">{review.content}</p>
-    </Card>
+    <>
+      <div className="bg-white/5 rounded-xl p-8 border-2 border-white/20">
+        <div className="naslovi flex">
+          <img
+            src="https://api.dicebear.com/7.x/miniavs/svg?seed=8"
+            alt={review.author}
+            className="h-10"
+          />
+          <div className="pl-2">
+            <div className="font-medium">{review.author}</div>
+            <div className="font-thin text-gray-300">
+              {format(review.created_at, "dd.MM.yyyy.")}
+            </div>
+          </div>
+        </div>
+        <div className="px-2 py-1 rounded-lg w-max bg-white/10 mt-4 font-medium flex gap-2">
+          <StarFilled style={{ color: "#ffda03" }} />
+          {review.author_details.rating} / 10
+        </div>
+        <div className="mt-2 text-base/8 line-clamp-3">{review.content}</div>
+      </div>
+    </>
   );
 };
 
