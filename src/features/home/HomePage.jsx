f47969879b3c79a854d88ec8fd0db450f2acf143
karlafehir <fehirkarla@gmail.com>
2025-05-05 10:09:17 +0200

    added antd input for movie search

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,24 +1,16 @@
 import MovieCard from "../../components/cards/MovieCard";
-import { useDispatch } from "react-redux";
-import { search } from "../../store/searchMoviesSlice";
+import { Input } from "antd";
 
-const HomePage = ({ movies }) => {
-  const dispatch = useDispatch();
+const HomePage = ({ movies, onSearch }) => {
+  const { Search } = Input;
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.target);
-          const searchParam = formData.get("title") ?? "";
-          dispatch(search(searchParam));
-        }}
-      >
-        <label htmlFor="title">
-          <input id="title" name="title" placeholder="Search by title" />
-        </label>
-      </form>
+      <Search
+        placeholder="Search movies by title"
+        onSearch={onSearch}
+        style={{ width: 300 }}
+      />
 
       {movies && (
         <div className="flex gap-4 flex-wrap">
--- src/features/home/HomePageContainer.jsx
+++ src/features/home/HomePageContainer.jsx
@@ -1,17 +1,25 @@
 import HomePage from "./HomePage";
 import { useGetPopularMoviesQuery } from "../../store/movieApiService";
 import { useSearchMoviesQuery } from "../../store/movieApiService";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { search } from "../../store/searchMoviesSlice";
 
 const HomePageContainer = () => {
   const { data: movies } = useGetPopularMoviesQuery();
 
+  const dispatch = useDispatch();
+
+  const onSearch = (value, _e) => {
+    dispatch(search(value));
+  };
+
   const searchMovieParams = useSelector((state) => state.searchMovies.value);
   let { data: searchedMovies } = useSearchMoviesQuery(searchMovieParams);
+
   const moviesToDisplay =
     searchedMovies?.length === 0 ? movies : searchedMovies;
 
-  return <HomePage movies={moviesToDisplay} />;
+  return <HomePage movies={moviesToDisplay} onSearch={onSearch} />;
 };
 
 export default HomePageContainer;
