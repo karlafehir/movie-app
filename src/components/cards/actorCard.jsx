00e3c5f649dded394c40c16f074380461551e941
karlafehir <fehirkarla@gmail.com>
2025-05-07 14:35:19 +0200

    actor card - added default img & design fix

--- src/components/cards/actorCard.jsx
+++ src/components/cards/actorCard.jsx
@@ -1,9 +1,13 @@
 const ActorCard = ({ actor }) => {
   return (
-    <div className="rounded-xl w-30 h-64">
+    <div className="rounded-xl w-30">
       <img
-        className="object-cover rounded-lg"
-        src={"https://image.tmdb.org/t/p/original" + actor.profile_path}
+        className="object-cover rounded-lg w-32 h-48"
+        src={` ${
+          actor.profile_path
+            ? "https://image.tmdb.org/t/p/original" + actor.profile_path
+            : "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+        }`}
         alt={actor.name}
       />
       <div className="pt-2 pb-4">
