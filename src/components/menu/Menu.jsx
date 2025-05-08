0f884b9b2e55f23b74ae9572c956a3fc6b89b2f8
karlafehir <fehirkarla@gmail.com>
2025-05-08 13:16:44 +0200

    menu - keep selectedKey after refresh fix

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,33 +5,34 @@ import {
 } from "@ant-design/icons";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const items = [
-  { key: "1", icon: <DesktopOutlined />, label: <Link to="/">Home</Link> },
+  { key: "/", icon: <DesktopOutlined />, label: <Link to="/">Home</Link> },
   {
-    key: "2",
+    key: "/favorites",
     icon: <HeartOutlined />,
     label: <Link to="/favorites">Favorites</Link>,
   },
   {
-    key: "3",
+    key: "/genres",
     icon: <StarOutlined />,
     label: <Link to="/genres">Genres</Link>,
   },
   {
-    key: "4",
+    key: "/actors",
     icon: <StarOutlined />,
     label: <Link to="/actors">Actors</Link>,
   },
 ];
 
 const MenuComponent = () => {
+  const location = useLocation();
   return (
     <div className="w-72 h-screen overflow-hidden flex-shrink-0">
       <Menu
         className="h-full"
-        defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub1"]}
+        selectedKeys={[location.pathname]}
         mode="inline"
         theme="dark"
         items={items}
